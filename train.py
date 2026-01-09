3af322e3d5cfa95c91fa975a78fcc0d6651ded1e
2022BCS0115-atharv <atharvmishra339@gmail.com>
2026-01-09 05:49:29 +0000

    Model-Ridge, alpha-1.0, testsplit-0.2, preprocessing-scaler

--- train.py
+++ train.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import Ridge
+
 
 
 # -----------------------------
@@ -48,7 +50,7 @@ X_test = scaler.transform(X_test)
 # -----------------------------
 # Model Training
 # -----------------------------
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 
