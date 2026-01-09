710279dcb25edfcad98dd90586f01bbd0c62588c
2022BCS0115-atharv <atharvmishra339@gmail.com>
2026-01-09 05:57:34 +0000

    Model-LinearRegression, testsplit-0.3, preprocessing-scaler

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import Lasso, LinearRegression
 
 
 
@@ -34,7 +34,7 @@ y = data["quality"]
 # Train-test split
 # -----------------------------
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y, test_size=0.3, random_state=42
 )
 
 
@@ -49,7 +49,7 @@ X_test = scaler.transform(X_test)
 # -----------------------------
 # Model Training
 # -----------------------------
-model = Lasso(alpha=0.1, max_iter=10000)
+model = LinearRegression()
 model.fit(X_train, y_train)
 
 
